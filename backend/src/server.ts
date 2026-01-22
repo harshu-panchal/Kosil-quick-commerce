02ef36bb0709b4452afc9fc3f21f3562502111b7
harshu-panchal <harshvardhanpanc145@gmail.com>
2026-01-22 18:40:40 +0530

    cors policy updated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,6 +21,8 @@ const allowedOrigins = [
   "https://www.kosil.com",
   "https://kosil.com",
   "https://kosil-frontend.onrender.com",
+  "https://kosil.biz",
+  "https://kosil.biz/",
   // Add more origins from environment variable if needed
   ...(process.env.FRONTEND_URL
     ? process.env.FRONTEND_URL.split(",").map((url) => url.trim())
